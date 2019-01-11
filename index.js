1a1aad783c80eec06c172cc94a20e07f2d0e5cf8
Gracie McGuire <m.graciemcguire@gmail.com>
2019-01-11 18:23:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let ranked = document.querySelectorAll('ul.ranked-list li')
+  
+  for(let i = 0; i < ranked.length; i++){
+    ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n;
+    
+  }
+}
+
+
+function deepestChild(){
+  let kids = document.querySelectorAll('#grand-node div');
+  
+  for (let i = 0; i < kids.length; i++){
+    if (!kids.length){
+    return false;
+  }
+  return kids[kids.length-1];
+ }
+}
